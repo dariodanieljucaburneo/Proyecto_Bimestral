90e40d80840f8556bdfa86b2a32bbcafa17cf0c5
IsmaelS-code <ismaelsarangolojan@gmail.com>
2026-07-20 09:59:30 -0500

    utilidades

---
+++ streamflow/src/streamflow/StreamFlow.java
@@ -0,0 +1,13 @@
+package streamflow;
+
+import Vista.VistaStreamFlow;
+
+public class StreamFlow {
+
+    public static void main(String[] args) {
+
+        new VistaStreamFlow().menu();
+        
+    }
+    
+}
